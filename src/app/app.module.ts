05d1d8a6aff2cf0b9c6e834c8abed43375d30123
allan-mlpe <allan_mlpe@hotmail.com>
2017-08-14 21:19:47 -0300

    Inclusão da lógica de negócio para cálculo de horas restantes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CalculadoraEstagioComponent } from './calculadora-estagio/calculadora-estagio.component';
@@ -10,7 +11,8 @@ import { CalculadoraEstagioComponent } from './calculadora-estagio/calculadora-e
     CalculadoraEstagioComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/calculadora-estagio/calculadora-estagio.component.ts
+++ src/app/calculadora-estagio/calculadora-estagio.component.ts
@@ -9,10 +9,28 @@ export class CalculadoraEstagioComponent implements OnInit {
 
 
   private remainingHours: number;
+  private totalHours: number = 450;
+  private completedHours: number = 0;
+  private missedHours: number = 0;
 
   constructor() { }
 
   ngOnInit() {
+    this.updateRemainingHours();
   }
 
+  changeTotalHours(value) {
+    //console.log(value);
+    this.totalHours = value;
+    this.updateRemainingHours();
+  }
+
+  changeCompletedHours(value) {
+    this.completedHours = value;
+    this.updateRemainingHours();
+  }
+
+  updateRemainingHours() {
+    this.remainingHours = this.totalHours - this.completedHours;
+  }
 }
